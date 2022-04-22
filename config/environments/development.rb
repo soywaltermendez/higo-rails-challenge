b2afd39702d5b36ed5769515abf3b59eae394265
Walter Guillermo Mendez Villanueva <walter.mendez.michelada@kueski.com>
2022-04-22 12:33:20 -0500

    [TESTS] Solved assets precompile error for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.logger = Logger.new("log/development.log")
+  config.log_level = :info
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
