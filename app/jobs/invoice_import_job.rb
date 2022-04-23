80a2c58acd49e44ffe98c52b858bcfec73c9fcdb
Walter Guillermo Mendez Villanueva <walter.mendez.michelada@kueski.com>
2022-04-23 11:31:14 -0500

    [JOB] Added Job validation

--- app/jobs/invoice_import_job.rb
+++ app/jobs/invoice_import_job.rb
@@ -8,6 +8,7 @@ class InvoiceImportJob < ApplicationJob
   def perform(files, current_user)
     files.each do |xml|
       ActiveRecord::Base.transaction do
+        validate_xml(xml)
         handle_xml(xml, current_user)
       end
     end
@@ -20,6 +21,39 @@ class InvoiceImportJob < ApplicationJob
   end
 
   private
+
+  def validate_xml(xml)
+    if xml.has_key?(:hash)
+      xml_json = xml[:hash]
+      validate_invoice_info(:invoice_uuid, xml_json)
+      validate_invoice_info(:amount_cents, xml_json)
+      validate_invoice_info(:amount_currency, xml_json)
+      validate_invoice_info(:emitted_at, xml_json)
+      validate_invoice_info(:expires_at, xml_json)
+      validate_person(:emitter, xml_json, xml_json[:emitter])
+      validate_person(:receiver, xml_json, xml_json[:receiver])
+    else
+      raise StandardError, "Missing hash key"
+    end
+  end
+
+  def validate_person(key, hash, person_hash)
+    if hash.has_key?(key)
+      if person_hash.has_key?(person_hash[:name]) || !person_hash[:name].present?
+        raise StandardError, "Missing #{key} name"
+      elsif person_hash.has_key?(person_hash[:rfc]) || !person_hash[:rfc].present?
+        raise StandardError, "Missing #{key} rfc"
+      end
+    else
+      raise StandardError, "Missing #{key}"
+    end
+  end
+
+  def validate_invoice_info(key, hash)
+    if !hash.has_key?(key) || !hash[key].present?
+      raise StandardError, "Missing #{key}"
+    end
+  end
 
   def handle_xml(xml, current_user)
     xml_json = xml[:hash]
